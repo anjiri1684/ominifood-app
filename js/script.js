67d97d3dc023bda1d5f175bee553a62b301e9e31
vincent anjiri <vincentanjiri12@gmail.com>
2024-07-18 16:39:27 +0300

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,69 @@
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
 
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
   console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
 
-const date = (document.getElementById("date").innerHTML =
+const date = (document.getElementById('date').innerHTML =
   new Date().getFullYear());
 
 //make mobile navigation work
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
-const btnNavEl = document.querySelector(".btn-mobile-nav");
+const btnNavEl = document.querySelector('.btn-mobile-nav');
 
-const headerEl = document.querySelector(".header");
+const headerEl = document.querySelector('.header');
 
-btnNavEl.addEventListener("click", function () {});
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+// Smooth scrolling
+
+const allLinks = document.querySelectorAll('a:link, a:visited');
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to the top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // scroll to other links
+
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelectorAll(href);
+      sectionEl.scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains('man-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  });
+});
+
 /*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
   margin-right: 4.8rem;
